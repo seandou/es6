106f27897b79788153842131ba22b8186982320e
Sean Dou <douguohui@gmail.com>
2015-11-06 16:41:33 +0800

    code style zh doc

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # es6
+
+
+Resources
+============
+
+https://www.npmjs.com/package/mongoose-class-wrapper
+
+[https://github.com/lukehoban/es6features](https://github.com/lukehoban/es6features)
--- code-style.js
+++ code-style.js
@@ -1,5 +1,6 @@
 /**
  * References
+ * http://es6.ruanyifeng.com/#docs/style
  * http://www.iteye.com/news/28028-JavaScript-code-style-guide
  * node: http://nodeguide.com/style.html
  * google: https://google.github.io/styleguide/javascriptguide.xml
