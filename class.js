b1e6294aebc09414ad7a3a3bbbb425e4dc100072
Sean Dou <douguohui@gmail.com>
2015-11-06 10:46:16 +0800

    class.js

--- class.js
+++ class.js
@@ -4,6 +4,8 @@
  * References
  * http://www.ruanyifeng.com/blog/2013/01/javascript_strict_mode.html
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set
  */
 class Point {
   constructor(x, y) {
