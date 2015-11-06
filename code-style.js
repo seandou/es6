3f8237d82d5fae49ff03630478a28139b4c6a165
Sean Dou <douguohui@gmail.com>
2015-11-06 16:42:50 +0800

    airbnb javascript code style

--- code-style.js
+++ code-style.js
@@ -4,4 +4,5 @@
  * http://www.iteye.com/news/28028-JavaScript-code-style-guide
  * node: http://nodeguide.com/style.html
  * google: https://google.github.io/styleguide/javascriptguide.xml
+ * airbnb: https://github.com/airbnb/javascript
  */
