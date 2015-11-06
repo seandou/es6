b22b1f9dd73a528e967f7a580ce731ad6c97f049
Sean Dou <douguohui@gmail.com>
2015-11-06 10:39:59 +0800

    code style

---
+++ code-style.js
@@ -0,0 +1,6 @@
+/**
+ * References
+ * http://www.iteye.com/news/28028-JavaScript-code-style-guide
+ * node: http://nodeguide.com/style.html
+ * google: https://google.github.io/styleguide/javascriptguide.xml
+ */
